293db7674118e4b5f2274f21c9a7115e8f810998
suraj kumar <surajkumar.sk76@gmail.com>
2024-03-15 00:23:37 +0530

    fixed the button and error

--- src/hooks/counter.tsx
+++ src/hooks/counter.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 const useCounter = () => {
   const [count, setCount] = useState(0);
-  const increament = () => {
+  const increment = () => {
     setCount(count + 1);
   };
   const decrement = () => {
     setCount(count - 1);
   };
-  return { count, increament, decrement };
+  return { count, increment, decrement };
 };
 export { useCounter };
